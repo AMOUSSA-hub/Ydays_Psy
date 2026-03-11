e6326cdb7ff9e4180eef6f8de622a89044b08788
[dyad] <git@dyad.sh>
2026-03-11 23:07:56 +0100

    [dyad] Fixed Babel bundling error by disabling React Compiler experiment and explicitly resolving NativeWind plugin. - wrote 2 file(s)

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel'],
+    plugins: [
+      // Ensure nativewind/babel is the last plugin and explicitly resolved
+      require.resolve('nativewind/babel'),
+    ],
   };
 };
